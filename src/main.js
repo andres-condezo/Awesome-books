b5742996f5b3d1ce0608d990fc2f18169cc3179d
joseheco <jferhc@gmail.com>
2022-01-03 16:12:05 -0500

    add function addbook and loop of array

--- src/main.js
+++ src/main.js
@@ -1,15 +1,57 @@
 #!/usr/bin/env node
 
-let bookCollection = [];
+let bookCollection = [
+	{
+		title: 'Book 1',
+		author: 'jose',
+		id: 0
+	},
+	{
+		title: 'Book 2',
+		author: 'andres',
+		id: 1
+	},
+	{
+		title: 'Book 3',
+		author: 'microverse',
+		id: 2
+	},
+];
 
 function addBook() {
-	console.log("book added");
+
 }
 
 function removeBook() {
 	console.log("book added");
 }
 
-function displayBookCollection() {
-	console.log("bookCollection displayed");
+function createBook(el) {
+return `
+<h3 class="bookTitle">${el.title}</h3>
+<p class="bookAuthor">${el.author}</p>
+<button type='button' class="addBook">Remove</button>`
+
 }
+
+
+function displayBookCollection() {
+}
+
+const bookContainer = document.querySelector('#book-container');
+
+const myBooks = bookCollection.map( (el)=>{
+		const article = document.createElement('article');
+		article.className= 'article-book';
+		article.innerHTML = createBook(el);
+		bookContainer.appendChild(article);
+	}
+)
+	
+
+
+// for(let i = 0; i < myBooks.length; i+=1) {
+// 	bookContainer(myBooks[i]);
+// 	console.log('3')
+// }
+	
