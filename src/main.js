82e57fff0d055d662faa79f2242ec06533714b82
andres-condezo <ra.condezomonge@gmail.com>
2022-01-04 04:15:41 -0500

    Feat(main.js): [Create remove books function]

--- src/main.js
+++ src/main.js
@@ -1,71 +1,90 @@
-#!/usr/bin/env node
+// *****************
+// Global variables
+// *****************
 
-let bookCollection = [];
-
-const $ = selector => document.querySelector(selector) 
+const $ = (selector) => document.querySelector(selector);
 
 const $titleInput = $('#titleInput');
 const $authorInput = $('#authorInput');
 const $addBookBtn = $('#addBookBtn');
+const $bookContainer = $('#book-container');
 
-let bookId = bookCollection.length;
+let bookCollection = [];
 
+// ***************
+// Local Storage
+// ***************
 
-function catchLocal() {
-	const catchCollection = JSON.stringify(bookCollection);
-	const catchId = JSON.stringify(bookId);
-
-	localStorage.setItem('bookCollection', catchCollection);
-	localStorage.setItem('bookId', catchId);
+function saveLocal() {
+  const catchCollection = JSON.stringify(bookCollection);
+  localStorage.setItem('bookCollection', catchCollection);
 }
-
 
 function getLocal() {
-	if (localStorage.getItem('bookCollection')){
-	bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
-	bookId = JSON.parse(localStorage.getItem('bookId'));
-	catchLocal();
-	}
+  if (localStorage.getItem('bookCollection')) {
+    bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
+  }
 }
 
+// ***************
+// main functions
+// ***************
 
-function addBook() {
-	bookCollection.push({title: $titleInput.value, author: $authorInput.value, id: bookId});
-	displayBookCollection();
-	bookId += 1;
-	catchLocal();
-}
-
-getLocal();
-window.onload = getLocal();
-
-$addBookBtn.addEventListener('click', addBook)
-
-
-function removeBook() {
-
-	catchLocal();
-}
-
-function createBook(el) {
-return `
+function bookTemplate(el) {
+  return `
 <h3 class="bookTitle">${el.title}</h3>
 <p class="bookAuthor">${el.author}</p>
-<button type='button' class="addBook">Remove</button>`
+<button type='button' class="createRemoveBtnBtn">Remove</button>
+`;
 }
 
+function renderBooks() {
+  bookCollection.forEach((el) => {
+    const article = document.createElement('article');
+    article.className = 'article-book';
+    article.innerHTML = bookTemplate(el);
+    $bookContainer.appendChild(article);
+  });
+}
+
+function createRemoveBtn() {
+  const $removeBookBtn = document.querySelectorAll('.removeBookBtn');
+  $removeBookBtn.forEach((el, index) => {
+    el.addEventListener('click', () => {
+      bookCollection.splice(index, 1);
+      renderBooks();
+      saveLocal();
+    });
+  });
+}
 
 function displayBookCollection() {
-	const bookContainer = document.querySelector('#book-container');
-	bookContainer.innerHTML = '';
-
-	bookCollection.map( (el)=>{
-			const article = document.createElement('article');
-			article.className= 'article-book';
-			article.innerHTML = createBook(el);
-			bookContainer.appendChild(article);
-		}
-		)
+  $bookContainer.innerHTML = '';
+  renderBooks();
+  createRemoveBtn();
 }
 
-displayBookCollection();
+function clearFields() {
+  $titleInput.value = '';
+  $authorInput.value = '';
+}
+
+function addBook() {
+  if ($titleInput.value && $authorInput.value) {
+    bookCollection.push({
+      title: $titleInput.value,
+      author: $authorInput.value,
+    });
+    displayBookCollection();
+    saveLocal();
+    clearFields();
+  }
+}
+
+function main() {
+  getLocal();
+  displayBookCollection();
+  $addBookBtn.addEventListener('click', addBook);
+}
+
+main();
